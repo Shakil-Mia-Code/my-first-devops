226b4deccecee6905ef54fc7185a8e6ba59e0e8c
shakil <shahadatshakh666@gmail.com>
2026-08-14 23:10:29 +0300

    Successfully completed my first real  DevOps project - Automated cloud server monitor

--- script.py
+++ script.py
@@ -1,4 +1,12 @@
-engineer_name = input("Enter your name: ")
-server_ip = input("Enter terget  Server Ip: ")
-print(f"\n[SYSTEM REPORT] ")
-print(f"Hello {engineer_name}, connecting to cloud server at  {server_ip}...")
+def check_server_health(engineer, ip, cpu):
+    print(f"\n--- [LIVE CLOUD REPORT {ip} ---")
+    print(f" Monitored By: {engineer}")
+    if cpu > 80:
+        print(f" STATUS; [ ALERT ] -> CPU load is {cpu}%. Server is overloading!")
+    else:
+        print(f" STATUS: [ HEALTHY ] -> CPU load is  {cpu}%. All systems is normal.")
+print ("=== Welcome to DevOps Cloud Monitor ===")
+name = input("Enter Your Name: ")
+target_ip = input("Entet Server Ip Address: ")
+cpu_input = int(input("Enter Current Cpu Usage (0-100): "))
+check_server_health(name, target_ip, cpu_input)
