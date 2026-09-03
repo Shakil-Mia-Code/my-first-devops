d03ad48cde4f83a999a7a78d8e0cb5f02fcdc95b
shakil <shahadatshakh666@gmail.com>
2026-09-03 23:06:18 +0300

    Mastered first devops python scripting file and dynamic f-string analytics

---
+++ server_report.py
@@ -0,0 +1,9 @@
+print("============================")
+print(" LIVE CLOUD SERVER MONITOR ")
+print("============================")
+
+
+total_ram = 16
+used_ram = 9
+
+print(f"Free Server RAM Memory: {total_ram - used_ram} GB")
