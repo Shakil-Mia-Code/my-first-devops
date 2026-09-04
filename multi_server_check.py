0e1af834ab67419bc0e355bbdd72da71dd757329
shakil <shahadatshakh666@gmail.com>
2026-09-04 22:48:55 +0300

    Mastered advanced python for-loop script for cloud multi-server infrastructure auditin

---
+++ multi_server_check.py
@@ -0,0 +1,16 @@
+server_ips = ["192.168.1.1", "10.0.0.5", "172.0.16.22", "100.156.5.7", "125.34.12.0"]
+
+print("\n=============================================")
+print(" MEGA CLOUD INFRUSTRUCTURE REPORT  ")
+print("==============================================")
+
+for ip in server_ips:
+    print(f"Pinging Terget Server Node -> [ {ip} ]")
+    print(f"STATUS: ONLINE | Connection Secured.\n")
+
+
+print("===============================================")
+print("ALL TERGET SERVERS VARIFIED SUCCESSFULLY.")
+print("================================================")
+
+print("           Monitoring By | SHAKIL        ")
