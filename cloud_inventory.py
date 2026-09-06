3a3ff832086f5bea62e70dc23f5018fe71b14368
shakil <shahadatshakh666@gmail.com>
2026-09-06 22:39:55 +0300

    Mastered advanced python dictionary data structures for enterprise cloud inventory automation

---
+++ cloud_inventory.py
@@ -0,0 +1,22 @@
+server_profile = {
+    "host_name"   : "Riyad-prod-node-01",
+    "ip_address"  : "127.0.0.1",
+    "environment" : "production",
+    "ram_gb"      : 16.
+
+}
+
+print("\n================================================")
+print("     CENTRAL CLOUD & EVENTORY DASHBOARD     ")
+print("================================================")
+
+
+print(f"Terget Hostnam:  {server_profile[ 'host_name'  ]}") 
+print(f"Server IP     : { server_profile[ 'ip_address' ]}")
+print(f"Terget Laywer : {server_profile[ 'environment' ]}")
+
+print("==================================================")
+print("      INVENTORY STATUS : ACTIVE & VERIFIED        ")
+print("=================================================\n")
+
+print("            Check By | Engineer SHAKIL      \n")
